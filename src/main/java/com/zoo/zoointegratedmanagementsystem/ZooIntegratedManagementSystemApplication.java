63f3bf3ab87bebd2abd0f01de8dd17ec6a1fc957
Yuhua <1774630667@qq.com>
2025-12-02 17:27:54 +0800

    Initial commit

---
+++ src/main/java/com/zoo/zoointegratedmanagementsystem/ZooIntegratedManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zoo.zoointegratedmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooIntegratedManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooIntegratedManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoo/zoointegratedmanagementsystem/ZooIntegratedManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoo.zoointegratedmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooIntegratedManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
